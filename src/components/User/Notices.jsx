6bf1e7f6a857094295a1fcb019524f88d52f1a04
marditds <marditds@gmail.com>
2024-09-05 23:02:28 -0700

    ⏳ Display modification
    ✅ Changed the way time ⏳ is displayed on the UI.

--- src/components/User/Notices.jsx
+++ src/components/User/Notices.jsx
@@ -11,29 +11,49 @@ export const Notices = ({ notices, handleEditNotice, handleDeleteNotice, removin
     return (
         <>
             {notices.map((notice, idx) => (
-                <p
-                    key={idx}>
-                    {notice.text}
-                    <br />
-                    <small>
-                        {formatDateToLocal(notice.timestamp)}
-                    </small>
-                    <br />
-                    <small>
-                        {formatDateToLocal(notice.expiresAt)}
-                    </small>
-                    <br />
-                    <Button onClick={() => handleEditNotice(notice.$id, notice.text)}>
-                        <AiFillEdit size={20} />
-                    </Button>
-                    <Button onClick={() => handleDeleteNotice(notice.$id)} disabled={removingNoticeId === notice.$id}>
-                        {removingNoticeId === notice.$id ?
-                            <Loading /> :
-                            <CgTrash size={20} />
-                        }
-                    </Button>
+                <div
+                    key={idx}
+                    className='d-grid mt-2'
+                >
+                    <hr />
 
-                </p>
+                    <p className='d-flex w-100 mb-2'>
+                        {notice.text}
+
+
+                        <Button
+                            className='ms-auto'
+                            onClick={() => handleEditNotice(notice.$id, notice.text)}
+                        >
+                            <AiFillEdit size={20} />
+                        </Button>
+                        <Button
+                            className='ms-2'
+                            onClick={() => handleDeleteNotice(notice.$id)} disabled={removingNoticeId === notice.$id}
+                        >
+                            {removingNoticeId === notice.$id ?
+                                <Loading /> :
+                                <CgTrash size={20} />
+                            }
+                        </Button>
+                    </p>
+
+                    <div className='d-flex'>
+
+                        <small className='me-auto'>
+                            <span style={{ color: 'gray' }}>
+                                Expires At:
+                            </span> {formatDateToLocal(notice.expiresAt)}
+                        </small>
+
+                        {/* <small>
+                            <span style={{ color: 'gray' }}>
+                                Created on:
+                            </span> {formatDateToLocal(notice.timestamp)}
+                        </small> */}
+
+                    </div>
+                </div>
             ))}
         </>
     );
--- src/lib/utils/dateUtils.js
+++ src/lib/utils/dateUtils.js
@@ -1,4 +1,11 @@
 export const formatDateToLocal = (isoString) => {
     const date = new Date(isoString);
-    return date.toLocaleString();
+
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = date.toLocaleString('default', { month: 'short' });
+
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+
+    return `${month} ${day} ${hours}:${minutes}`;
 };
