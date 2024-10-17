0e18ba078c9c7cd1d52c52b6e1d595ace371bfc7
marditds <marditds@gmail.com>
2024-10-16 23:37:02 -0700

    Update main.js

--- functions/src/main.js
+++ functions/src/main.js
@@ -1,23 +1,31 @@
 import { Client, Users, ID } from 'node-appwrite';
 
-// const client = new Client()
-//   .setEndpoint(import.meta.env.VITE_ENDPOINT)
-//   .setProject(import.meta.env.VITE_PROJECT)
-//   .setKey(req.headers['x-appwrite-key'] ?? import.meta.env.VITE_SHORT_NOTICE_API_KEYS);
+const client = new Client()
+  .setEndpoint(process.env.VITE_ENDPOINT)
+  .setProject(process.env.VITE_PROJECT)
+  .setKey(req.headers['x-appwrite-key'] ?? process.env.VITE_SHORT_NOTICE_API_KEYS);
 
-// const users = new Users(client);
+const users = new Users(client);
+
+const newUser = await users.create(
+  ID.unique(), // userId
+  'my@mail.com', // email (optional)
+  '+13102548866', // phone (optional)
+  '',
+  'Hakopos' // name (optional)
+);
 
 // This Appwrite function will be executed every time your function is triggered
 export default async ({ req, res, log, error }) => {
   // You can use the Appwrite SDK to interact with other services
   // For this example, we're using the Users service
 
-  const client = new Client()
-    .setEndpoint(process.env.VITE_ENDPOINT)
-    .setProject(process.env.VITE_PROJECT)
-    .setKey(req.headers['x-appwrite-key'] ?? process.env.VITE_SHORT_NOTICE_API_KEYS);
+  // const client = new Client()
+  //   .setEndpoint(process.env.VITE_ENDPOINT)
+  //   .setProject(process.env.VITE_PROJECT)
+  //   .setKey(req.headers['x-appwrite-key'] ?? process.env.VITE_SHORT_NOTICE_API_KEYS);
 
-  const users = new Users(client);
+  // const users = new Users(client);
 
 
   try {
@@ -30,12 +38,12 @@ export default async ({ req, res, log, error }) => {
     //   'Hakopos' // name (optional)
     // );
 
-    // log(`User created successfully: ${newUser.$id}`);
+    log(`User created successfully: ${newUser.$id}`);
 
     const response = await users.list();
     // Log messages and errors to the Appwrite Console
     // These logs won't be seen by your end users
-    log(`Total users: ${response}`);
+    log(`Total users: ${response.length}`);
   } catch (err) {
     error("Could not list users: " + err.message);
   }
