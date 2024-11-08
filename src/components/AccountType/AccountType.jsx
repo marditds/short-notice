31ded977dfe37c83fe8f33f24da6076bae8ee18e
marditds <marditds@gmail.com>
2024-11-07 19:04:24 -0800

    🎨 Styling setUsername

--- src/components/AccountType/AccountType.jsx
+++ src/components/AccountType/AccountType.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form } from 'react-bootstrap';
 import { BsFillPersonFill } from "react-icons/bs";
 import { PiBagFill } from "react-icons/pi";
 import { BsBuildings } from "react-icons/bs";
 
 export const AccountType = ({ setAccountType }) => {
+
+    const [selectedType, setSelectedType] = useState(null);
+
+    const handleSelection = (type) => {
+        setAccountType(type);
+        setSelectedType(type);
+    };
+
+
     return (
         <Form.Group className='mb-3' controlId='accountType'>
             <Form.Label>Select Account Type:</Form.Label>
             <div className='d-flex justify-content-evenly'>
                 <label
-                    className='d-flex 
-                flex-column 
-                justify-content-center 
-                align-items-center 
-                createUsername__accountType--radio'
+                    className={`d-flex flex-column justify-content-center align-items-center createUsername__accountType--radio ${selectedType === 'personal' ? 'selected' : ''}`}
+                    onClick={() => handleSelection('personal')}
                 >
                     <BsFillPersonFill size={45} />
                     <Form.Check
@@ -22,17 +28,14 @@ export const AccountType = ({ setAccountType }) => {
                         label={'Personal'}
                         id={'Personal'}
                         name='accountType'
-                        onChange={() => setAccountType('personal')}
+                        onChange={() => handleSelection('personal')}
                         className='createUsername__accountType--radio-input'
                     />
                 </label>
 
                 <label
-                    className='d-flex 
-                flex-column 
-                justify-content-center 
-                align-items-center 
-                createUsername__accountType--radio'
+                    className={`d-flex flex-column justify-content-center align-items-center createUsername__accountType--radio`}
+
                 >
                     <PiBagFill size={45} />
                     <Form.Check
@@ -40,18 +43,15 @@ export const AccountType = ({ setAccountType }) => {
                         label={'Professional'}
                         id={'Professional'}
                         name='accountType'
-                        onChange={() => setAccountType('professional')}
+                        onChange={() => handleSelection('professional')}
                         className='createUsername__accountType--radio-input'
                         disabled
                     />
                 </label>
 
                 <label
-                    className='d-flex 
-                flex-column 
-                justify-content-center 
-                align-items-center 
-                createUsername__accountType--radio'
+                    className={`d-flex flex-column justify-content-center align-items-center createUsername__accountType--radio ${selectedType === 'organization' ? 'selected' : ''}`}
+                    onClick={() => handleSelection('organization')}
                 >
                     <BsBuildings size={45} />
                     <Form.Check
@@ -59,7 +59,7 @@ export const AccountType = ({ setAccountType }) => {
                         label={'Organization'}
                         id={'Organization'}
                         name='accountType'
-                        onChange={() => setAccountType('organization')}
+                        onChange={() => handleSelection('organization')}
                         className='createUsername__accountType--radio-input'
                     />
                 </label>
