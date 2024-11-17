fef0b519f80405cc6219bee13aafa73ef6da1047
marditds <marditds@gmail.com>
2024-11-16 21:50:42 -0800

    Update main.js

--- functions/deleteExpired/src/main.js
+++ functions/deleteExpired/src/main.js
@@ -1,22 +1,51 @@
-import { Client, Users } from 'node-appwrite';
+import { Client, Users, Databases } from 'node-appwrite';
 
 // This Appwrite function will be executed every time your function is triggered
 export default async ({ req, res, log, error }) => {
   // You can use the Appwrite SDK to interact with other services
   // For this example, we're using the Users service
   const client = new Client()
-    .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
-    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(req.headers['x-appwrite-key'] ?? '');
+    .setEndpoint(process.env.VITE_ENDPOINT)
+    .setProject(process.env.VITE_PROJECT)
+    .setKey(process.env.VITE_SHORT_NOTICE_API_KEYS);
+
+  const databases = new Databases(client);
+
   const users = new Users(client);
 
   try {
+    log('Request method: ' + req.method);
+    log('Request headers: ' + JSON.stringify(req.headers));
+    log('Raw body: ' + req.body);
+    log('Raw payload: ' + req.payload);
+
     const response = await users.list();
+
+    const res = await databases.listDocuments(
+      process.env.VITE_DATABASE,
+      process.env.VITE_NOTICES_COLLECTION
+    )
+
+    const notices = response.documents;
+    const now = new Date();
+
+    for (const notice of notices) {
+      const expiresAt = new Date(notice.expiresAt);
+
+      if (expiresAt <= now) {
+        // Delete notice if it is expired
+        await databases.deleteDocument(
+          process.env.VITE_DATABASE,
+          process.env.VITE_NOTICES_COLLECTION,
+          notice.$id);
+        console.log(`Deleted expired notice: ${notice.$id}`);
+      }
+    }
     // Log messages and errors to the Appwrite Console
     // These logs won't be seen by your end users
-    log(`Total users: ${response.total}`);
-  } catch(err) {
-    error("Could not list users: " + err.message);
+
+  } catch (err) {
+    console.error("Error deleting expired notices:", error);
   }
 
   // The req object contains the request data
