fcd72b413afef5ca822b89750542c58c22d80b1d
marditds <marditds@gmail.com>
2024-12-11 18:47:33 -0800

    Moving like/save deletiong
    ✅ Moved likes/saves deletion from deleteExpired function.

--- functions/deleteExpired/src/main.js
+++ functions/deleteExpired/src/main.js
@@ -42,39 +42,39 @@ export default async ({ req, res, log, error }) => {
 
       if (expiresAt <= now) {
 
-        const [likesRes, savesRes] = await Promise.all([
-          databases.listDocuments(
-            process.env.VITE_DATABASE,
-            process.env.VITE_LIKES_COLLECTION,
-            [Query.equal('notice_id', notice.$id)]
-          ),
-          databases.listDocuments(
-            process.env.VITE_DATABASE,
-            process.env.VITE_SAVES_COLLECTION,
-            [Query.equal('notice_id', notice.$id)]
-          )
-        ]);
+        // const [likesRes, savesRes] = await Promise.all([
+        //   databases.listDocuments(
+        //     process.env.VITE_DATABASE,
+        //     process.env.VITE_LIKES_COLLECTION,
+        //     [Query.equal('notice_id', notice.$id)]
+        //   ),
+        //   databases.listDocuments(
+        //     process.env.VITE_DATABASE,
+        //     process.env.VITE_SAVES_COLLECTION,
+        //     [Query.equal('notice_id', notice.$id)]
+        //   )
+        // ]);
 
-        const likes = likesRes.documents;
-        const saves = savesRes.documents;
+        // const likes = likesRes.documents;
+        // const saves = savesRes.documents;
 
-        await Promise.allSettled([
-          ...likes.map((like) =>
-            databases.deleteDocument(
-              process.env.VITE_DATABASE,
-              process.env.VITE_LIKES_COLLECTION,
-              like.$id
-            )
-          ),
+        // await Promise.allSettled([
+        //   ...likes.map((like) =>
+        //     databases.deleteDocument(
+        //       process.env.VITE_DATABASE,
+        //       process.env.VITE_LIKES_COLLECTION,
+        //       like.$id
+        //     )
+        //   ),
 
-          ...saves.map((save) =>
-            databases.deleteDocument(
-              process.env.VITE_DATABASE,
-              process.env.VITE_SAVES_COLLECTION,
-              save.$id
-            )
-          )
-        ]);
+        //   ...saves.map((save) =>
+        //     databases.deleteDocument(
+        //       process.env.VITE_DATABASE,
+        //       process.env.VITE_SAVES_COLLECTION,
+        //       save.$id
+        //     )
+        //   )
+        // ]);
 
 
         // const lks = await databases.listDocuments(
