44f71c4598f5f9be2fde92047e4d625f2e9e6320
marditds <marditds@gmail.com>
2025-01-14 23:54:28 -0800

    👷‍♂️ Adding sidenav to help-center data

--- src/pages/PreLogin/HelpCenter/HelpCenterInfo/HelpCenterData.jsx
+++ src/pages/PreLogin/HelpCenter/HelpCenterInfo/HelpCenterData.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { HelpCenterArrs } from './HelpCenterArrs';
-import { Container, ListGroup } from 'react-bootstrap';
+import { Container, Row, Col, ListGroup, Nav } from 'react-bootstrap';
 
 const HelpCenterData = () => {
 
     let { helpCenterTitlesPath, helpCenterDataPath } = useParams();
 
-    const { titleDataMapping, titlesDataMap } = HelpCenterArrs();
+    const { titlesMap, titleDataMapping, titlesDataMap } = HelpCenterArrs();
 
     useEffect(() => {
         console.log('gettingStartedPath', helpCenterDataPath);
@@ -15,26 +15,45 @@ const HelpCenterData = () => {
 
     return (
         <Container>
-            <h4> {
-                titleDataMapping[helpCenterTitlesPath]?.[helpCenterDataPath]
-                || 'Title not found'
-            }</h4>
+            <Row>
+                <Col>
+                    <Nav
+                        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+                        className='flex-column'
+                    >
+                        {titlesMap[helpCenterTitlesPath].map((title, idx) => {
+                            return (
+                                <Nav.Item key={idx}>
+                                    <Nav.Link as={Link} to={`help-center/${helpCenterTitlesPath}/${title.path}`}>
+                                        {title.header}
+                                    </Nav.Link>
+                                </Nav.Item>
+                            )
+                        })}
+                    </Nav>
+                </Col>
+                <Col>
+                    <h4> {
+                        titleDataMapping[helpCenterTitlesPath]?.[helpCenterDataPath]
+                        || 'Title not found'
+                    }</h4>
 
-            <div>
-                <p className='mb-2'>{titlesDataMap[helpCenterDataPath].intro}</p>
-                <ListGroup as='ol' className='help__center-data-list'>
-                    {titlesDataMap[helpCenterDataPath].steps.map((step, idx) => (
-                        <ListGroup.Item key={idx} as='li' className='help__center-data-list-item aaa'>
-                            <strong>
-                                {titlesDataMap[helpCenterDataPath].steps.indexOf(step) + 1}.
-                            </strong>
-                            {'  '}{step}
-                        </ListGroup.Item>
-                    ))}
-                </ListGroup>
-                <p className='mt-2'>{titlesDataMap[helpCenterDataPath].outro}</p>
-            </div>
-
+                    <div>
+                        <p className='mb-2'>{titlesDataMap[helpCenterDataPath].intro}</p>
+                        <ListGroup as='ol' className='help__center-data-list'>
+                            {titlesDataMap[helpCenterDataPath].steps.map((step, idx) => (
+                                <ListGroup.Item key={idx} as='li' className='help__center-data-list-item aaa'>
+                                    <strong>
+                                        {titlesDataMap[helpCenterDataPath].steps.indexOf(step) + 1}.
+                                    </strong>
+                                    {'  '}{step}
+                                </ListGroup.Item>
+                            ))}
+                        </ListGroup>
+                        <p className='mt-2'>{titlesDataMap[helpCenterDataPath].outro}</p>
+                    </div>
+                </Col>
+            </Row>
         </Container>
     )
 }
