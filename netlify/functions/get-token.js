e73408f32e680f8d848dc64dcdec8bd6202c019a
marditds <marditds@gmail.com>
2025-03-03 17:41:05 -0800

    testing

--- netlify/functions/get-token.js
+++ netlify/functions/get-token.js
@@ -2,6 +2,6 @@ exports.handler = async () => {
     return {
         statusCode: 200,
         headers: { "Content-Type": "application/json" }, // Ensure JSON response
-        body: JSON.stringify({ token: process.env.GOOGLE_API_TOKEN || "MISSING_ENV_VAR" }) // Avoid undefined values
+        body: JSON.stringify({ token: process.env.GOOGLE_API_TOKEN || "MISSING_ENV_VAR" }) // Debugging fallback
     };
 };
