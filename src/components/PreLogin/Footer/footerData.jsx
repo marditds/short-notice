6da0a0db686b36d87da94d8737244cb67cff82cf
marditds <marditds@gmail.com>
2025-03-03 18:37:56 -0800

    test

--- src/components/PreLogin/Footer/footerData.jsx
+++ src/components/PreLogin/Footer/footerData.jsx
@@ -1,4 +1,4 @@
-export const footerData = {
+const footerData = {
     company: [
         { name: "About", url: "/about" }
         // { name: "Blog", url: "/blog" }
@@ -27,39 +27,4 @@ export const footerData = {
     developer: <span>Developed by <a target='_blank' href='https://marditds.netlify.app'>Mardit DerSoukiassian</a>.</span>
 };
 
-//     navigationLinks: [
-//         { name: "Home", url: "/" },
-//         { name: "About", url: "/about" },
-//         { name: "SN Plus", url: "/sn-plus" },
-//         { name: "Terms of Service", url: "/tos" },
-//         { name: "Privacy Policy", url: "/privacy" },
-//         { name: "Community Guidelines", url: "/community-guidelines" },
-//         { name: "Help Center", url: "/help-center" },
-//         { name: "Attributions", url: "/attributions" },
-//         { name: "Contact", url: "/contact" }
-//     ],
-
-//     socialLinks: [
-//         {
-//             id: 3, name: "X", url: "https://x.com", icon: <i className='bi bi-twitter-x' />
-//         },
-//         {
-//             id: 2, name: "Instagram", url: "https://instagram.com", icon: <i className='bi bi-instagram' />
-//         },
-//         {
-//             id: 1, name: "Facebook", url: "https://facebook.com", icon: <i className='bi bi-facebook' />
-//         }
-//     ],
-
-//     copyright: `© ${new Date().getFullYear()} ShortNotice. All rights reserved.`,
-
-//     developer: <span>Developed by <Link target='_blank' to='https://marditds.netlify.app'>Mardit DerSoukiassian</Link>.</span>
-
-// };
-
-// --------------------------------------------------
-// | Home | About | Contact | Privacy | Terms | Help |
-//     --------------------------------------------------
-//         Follow Us: [FB][IG][X][YT]
-//         © 2024 AppName.All rights reserved.
-// --------------------------------------------------
+export default footerData;
