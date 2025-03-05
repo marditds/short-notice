aeb9066af0989153351733395b1eb6ff5d708148
marditds <marditds@gmail.com>
2025-03-04 16:37:06 -0800

    Update gemini.js

--- src/lib/context/gemini.js
+++ src/lib/context/gemini.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const [geminiKey, setGeminiKey] = useState(null);
