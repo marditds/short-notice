ea7b3e055187b40eecc0f33005512b2f3a37ef62
marditds <marditds@gmail.com>
2025-03-08 18:23:38 -0800

    👷‍♂️ footer modification

--- src/components/PreLogin/Footer/Footer.jsx
+++ src/components/PreLogin/Footer/Footer.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { Container, Row, Col, Image } from 'react-bootstrap';
-import { footerData } from './FooterData.jsx';
+import { footerData } from './footerData.js';
 import { screenUtils } from '../../../lib/utils/screenUtils.js';
 import snLogo from '../../../assets/sn_long.png';
 import './Footer.css';
@@ -11,8 +11,6 @@ const Footer = () => {
     const { isMediumScreen } = screenUtils();
 
     const copyrightText = footerData.copyright.split('newLine');
-
-    console.log('copyrightText', copyrightText);
 
     const copyrightTextBreak = isMediumScreen && footerData.copyright.replace('newLine', '');
 
@@ -45,23 +43,22 @@ const Footer = () => {
                     </Col>
 
                     {/* COMPANY */}
-                    {/* <Col className='footer__col'>
+                    <Col className='footer__col'>
                         <h4>Company</h4>
                         {
                             footerData.company.map((companyData, idx) => {
                                 return (
                                     <Link key={idx} to={companyData.url}>
-
                                         <h6>{companyData.name}</h6>
                                     </Link>
 
                                 )
                             })
                         }
-                    </Col> */}
+                    </Col>
 
                     {/* EXPLORE */}
-                    {/* <Col className='footer__col'>
+                    <Col className='footer__col'>
                         <h4>Explore</h4>
                         {
                             footerData.explore.map((exploreData, idx) => {
@@ -72,10 +69,10 @@ const Footer = () => {
                                 )
                             })
                         }
-                    </Col> */}
+                    </Col>
 
                     {/* LEGAL */}
-                    {/* <Col className='footer__col'>
+                    <Col className='footer__col'>
                         <h4>Legal</h4>
                         {
                             footerData.legal.map((legalData, idx) => {
@@ -86,10 +83,10 @@ const Footer = () => {
                                 )
                             })
                         }
-                    </Col> */}
+                    </Col>
 
                     {/* SUPPORT */}
-                    {/* <Col className='footer__col'>
+                    <Col className='footer__col'>
                         <h4>Support</h4>
                         {
                             footerData.support.map((supportData, idx) => {
@@ -100,31 +97,29 @@ const Footer = () => {
                                 )
                             })
                         }
-                    </Col> */}
+                    </Col>
 
                     {/* SOCIALS */}
-                    {/* <Col className='footer__col'>
+                    <Col className='footer__col'>
                         <h4>Socials</h4>
                         {
                             footerData.followUs.map((followUsData, idx) => {
                                 return (
                                     <Link key={idx} to={followUsData.url} target='_blank'>
                                         <h6>{followUsData.name}</h6>
+                                        {/* <i className={followUsData.icon} /> */}
                                     </Link>
                                 )
                             })
                         }
-                    </Col> */}
+                    </Col>
                 </Row>
 
                 {/* COPYRIGHT */}
                 <Row md={12} className='flex-column justify-content-center mt-3 mb-4'>
-                    {/* <Col className='footer__col d-flex justify-content-center'>
-                        {footerData.copyright}
-                    </Col> */}
-                    {/* <Col className='footer__col d-flex justify-content-center'>
-                        {footerData.developer}
-                    </Col> */}
+                    <Col className='footer__col d-flex justify-content-center'>
+                        <a href={footerData.developer[0].link} target='_blank'>{footerData.developer[0].text}</a>
+                    </Col>
                 </Row>
 
             </Container>
--- src/components/PreLogin/Footer/footerData.js
+++ src/components/PreLogin/Footer/footerData.js
@@ -3,26 +3,33 @@ export const footerData = {
         { name: "About", url: "/about" }
         // { name: "Blog", url: "/blog" }
     ],
+
     explore: [
         { name: "SN Plus", url: "/sn-plus" },
         { name: "Attributions", url: "/attributions" },
     ],
+
     legal: [
         { name: "Terms of Service", url: "/tos" },
         { name: "Privacy Policy", url: "/privacy" }
     ],
+
     support: [
         { name: "Help Center", url: "/help-center" },
         { name: "Community Guidelines", url: "/community-guidelines" },
         { name: "Contact", url: "/contact" }
     ],
+
     followUs: [
-        { name: 'Facebook', url: 'https://facebook.com/', icon: <i className='bi bi-facebook' /> },
-        { name: 'X', url: 'https://x.com/', icon: < i className='bi bi-twitter-x' /> },
-        { name: 'Instagram', url: 'https://instagram.com/', icon: <i className='bi bi-instagram' /> },
+        { name: 'Facebook', url: 'https://facebook.com/', icon: 'bi bi-facebook' },
+        { name: 'X', url: 'https://x.com/', icon: 'bi bi-twitter-x' },
+        { name: 'Instagram', url: 'https://instagram.com/', icon: 'bi bi-instagram' },
     ],
 
     copyright: `© ${new Date().getFullYear()} ShortNotice.newLine All rights reserved.`,
 
-    developer: <span>Developed by <a target='_blank' href='https://marditds.netlify.app'>Mardit DerSoukiassian</a>.</span>
+    developer: [
+        { text: 'Developed by Mardit DerSoukiassian', link: 'https://marditds.netlify.app' }
+    ]
+
 }; 
