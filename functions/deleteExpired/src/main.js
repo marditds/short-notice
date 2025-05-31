659e021a3d2f6705d4218f56fddea5b6dc2fba73
marditds <marditds@gmail.com>
2025-05-30 22:29:36 -0700

    Update main.js

--- functions/deleteExpired/src/main.js
+++ functions/deleteExpired/src/main.js
@@ -1,8 +1,7 @@
-import { Client, Databases, Query } from 'node-appwrite';
+import { Client, Databases } from 'node-appwrite';
 
-// This Appwrite function will be executed every time your function is triggered
 export default async ({ req, res, log, error }) => {
-  // For this example, we're using the Users service
+
   const client = new Client()
     .setEndpoint(process.env.VITE_ENDPOINT)
     .setProject(process.env.VITE_PROJECT)
@@ -11,20 +10,12 @@ export default async ({ req, res, log, error }) => {
   const databases = new Databases(client);
 
   try {
-    // log('Request method: ' + req.method);
-    // log('Request headers: ' + JSON.stringify(req.headers));
-    // log('Raw body: ' + req.body);
-    // log('Raw payload: ' + req.payload);
-
-
     const ntcs = await databases.listDocuments(
       process.env.VITE_DATABASE,
       process.env.VITE_NOTICES_COLLECTION
     )
 
     const notices = ntcs.documents;
-
-    // log(notices);
 
     const now = new Date();
 
@@ -44,87 +35,6 @@ export default async ({ req, res, log, error }) => {
           process.env.VITE_NOTICES_COLLECTION,
           notice.$id);
         log(`Deleted expired notice: ${notice.text}`);
-
-        // const [likesRes, savesRes] = await Promise.all([
-        //   databases.listDocuments(
-        //     process.env.VITE_DATABASE,
-        //     process.env.VITE_LIKES_COLLECTION,
-        //     [Query.equal('notice_id', notice.$id)]
-        //   ),
-        //   databases.listDocuments(
-        //     process.env.VITE_DATABASE,
-        //     process.env.VITE_SAVES_COLLECTION,
-        //     [Query.equal('notice_id', notice.$id)]
-        //   )
-        // ]);
-
-        // const likes = likesRes.documents;
-        // const saves = savesRes.documents;
-        // const likes = likesRes.documents;
-        // const saves = savesRes.documents;
-
-        // await Promise.allSettled([
-        //   ...likes.map((like) =>
-        //     databases.deleteDocument(
-        //       process.env.VITE_DATABASE,
-        //       process.env.VITE_LIKES_COLLECTION,
-        //       like.$id
-        //     )
-        //   ),
-        // await Promise.allSettled([
-        //   ...likes.map((like) =>
-        //     databases.deleteDocument(
-        //       process.env.VITE_DATABASE,
-        //       process.env.VITE_LIKES_COLLECTION,
-        //       like.$id
-        //     )
-        //   ),
-
-        //   ...saves.map((save) =>
-        //     databases.deleteDocument(
-        //       process.env.VITE_DATABASE,
-        //       process.env.VITE_SAVES_COLLECTION,
-        //       save.$id
-        //     )
-        //   )
-        // ]);
-
-
-        // const lks = await databases.listDocuments(
-        //   process.env.VITE_DATABASE,
-        //   process.env.VITE_LIKES_COLLECTION,
-        //   [Query.equal('notice_id', notice.$id)]
-        // );
-        // const likes = lks.documents; 
-        // log(likes); 
-        // await Promise.allSettled(
-        //   likes.map((like) =>
-        //     databases.deleteDocument(
-        //       process.env.VITE_DATABASE,
-        //       process.env.VITE_LIKES_COLLECTION,
-        //       like.$id
-        //     )
-        //   )
-        // );
-
-        // const svs = await databases.listDocuments(
-        //   process.env.VITE_DATABASE,
-        //   process.env.VITE_SAVES_COLLECTION,
-        //   [Query.equal('notice_id', notice.$id)]
-        // );
-        // const saves = savesRes.documents;  
-        // log(saves); 
-        // await Promise.allSettled(
-        //   saves.map((save) =>
-        //     databases.deleteDocument(
-        //       process.env.VITE_DATABASE,
-        //       process.env.VITE_SAVES_COLLECTION,
-        //       save.$id
-        //     )
-        //   )
-        // );
-
-
       }
     }
 
