aa4b8508f4be921538024b715a0c04ebc62f80e8
marditds <marditds@gmail.com>
2025-05-31 19:25:45 -0700

    Update main.js

--- functions/userDelete/src/main.js
+++ functions/userDelete/src/main.js
@@ -23,7 +23,7 @@ export default async ({ req, res, log, error }) => {
     const data = typeof req.body === 'string' ? JSON.parse(req.body) : req.body;
 
     log('data:', data);
-    log('data.$id:', data.$id)
+    log('data.$id:', data.$id);
 
 
     const result = await users.list();
