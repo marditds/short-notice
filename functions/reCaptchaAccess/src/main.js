ab7abf78d4821665ccd2ef468bd5f401e818ea70
marditds <marditds@gmail.com>
2025-06-02 13:45:14 -0700

    Update main.js

--- functions/reCaptchaAccess/src/main.js
+++ functions/reCaptchaAccess/src/main.js
@@ -24,9 +24,11 @@ export default async ({ req, res, log, error }) => {
             }
         );
 
+        log('This is verifyResponse in server-side:', verifyResponse);
+
         const data = await verifyResponse.json();
 
-        log(data);
+        log('This is data in server-side:', data);
 
         if (!data.success) {
             return res.json({ success: false, message: 'reCAPTCHA verification failed', errorCodes: data['error-codes'] });
