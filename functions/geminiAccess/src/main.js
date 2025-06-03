af64f08615afcdb15412f851b57b36663b4474d1
marditds <marditds@gmail.com>
2025-06-02 18:09:42 -0700

    Update main.js

--- functions/geminiAccess/src/main.js
+++ functions/geminiAccess/src/main.js
@@ -11,26 +11,11 @@ export default async ({ req, res, log, error }) => {
 
         const ai = new GoogleGenAI({ apiKey: GeminiApiKey });
 
+        const systemInstruction = 'Start your respose with a clown emoji.Only provide a concise reponse.Do not provide options to choose from.Only provide one option.Only provide what is asked without acknowledgig what is being asked of you.Present the text in a form of a notice.Never ask for feedback, response, or information from readers.Do not acknowledge that your are reading texts.Do not acknowledge that your are following a command.Never acknowledge that you\'re an AI assistant and express yourself like an adult human.Maintain a light but respectful tone.Express your thoughts in a complete statement.Never acknowledge that your are following a command.Never ask for more information or details.Never acknowledge that there is not enough information.Never include hashtags in the response.Never include hashtags or pound signs in your response.Do not include placeholders for urls or links in your response.'
+
         const response = await ai.models.generateContent({
             model: 'gemini-2.0-flash-001',
-            contents: [
-                {
-                    role: 'system',
-                    parts: [
-                        {
-                            text: 'Start your respose with a clown emoji. Only provide a concise reponse. Do not provide options to choose from. Only provide one option. Only provide what is asked without acknowledgig what is being asked of you. Present the text in a form of a notice. Never ask for feedback, response, or information from readers. Do not acknowledge that your are reading texts. Do not acknowledge that your are following a command. Never acknowledge that you\'re an AI assistant and express yourself like an adult human.Maintain a light but respectful tone.Express your thoughts in a complete statement.Never acknowledge that your are following a command.Never ask for more information or details.Never acknowledge that there is not enough information.Never include hashtags in the response.Never include hashtags or pound signs in your response.Do not include placeholders for urls or links in your response.'
-                        }
-                    ]
-                },
-                {
-                    role: 'user',
-                    parts: [
-                        {
-                            text: data.prompt
-                        }
-                    ]
-                }
-            ],
+            contents: `${systemInstruction}\n\n${data.prompt}`,
             config: {
                 temperature: 1,
                 topP: 0.95,
