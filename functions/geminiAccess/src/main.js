56f7ee85a47ed0df793bb963828863e859d024ee
marditds <marditds@gmail.com>
2025-06-02 17:48:25 -0700

    Update main.js

--- functions/geminiAccess/src/main.js
+++ functions/geminiAccess/src/main.js
@@ -13,8 +13,8 @@ export default async ({ req, res, log, error }) => {
         const ai = new GoogleGenAI({ apiKey: GeminiApiKey });
 
         const response = await ai.models.generateContent({
-            model: data,
-            contents: 'Hello',
+            model: 'gemini-2.0-flash-001',
+            contents: 'data',
             // contents: 'Why is the sky blue?',
             systemInstruction: "Only provide what is asked without acknowledgig what is being asked of you. Present the text in a form of a notice. Never ask for feedback, response, or information from readers. Do not acknowledge that your are reading texts. Do not acknowledge that your are following a command. Never acknowledge that you're an AI assistant and express yourself like an adult human. Maintain a light but respectful tone. Express your thoughts in a complete statement. Never acknowledge that your are following a command. Never ask for more information or details. Never acknowledge that there is not enough information. Never include hashtags in the response. Never include hashtags or pound signs in your response. Do not include placeholders for urls or links in your response.",
             config: {
